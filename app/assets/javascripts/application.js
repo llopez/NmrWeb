89e9df531ea928a65d23b90bb180e66452563a24
Luis Lopez <luigibyte@gmail.com>
2016-01-28 13:20:56 -0300

    design integrated :)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require jquery.1.7.2.min
+// require jquery_ujs
+// require turbolinks
 
 //= require respond.min
 //= require modernizr.custom.17475
