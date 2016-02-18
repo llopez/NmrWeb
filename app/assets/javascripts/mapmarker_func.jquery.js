60c2ee30ca09913a4893163c6333e0c06a25f70a
Luis Lopez <luigibyte@gmail.com>
2016-02-18 01:17:11 -0300

    map markers

--- app/assets/javascripts/mapmarker_func.jquery.js
+++ app/assets/javascripts/mapmarker_func.jquery.js
@@ -2,14 +2,16 @@ $(document).ready(function(){
 
 		//set up markers
 		var myMarkers = {"markers": [
-				{"latitude": "34.095085", "longitude":"-118.281671", "icon": "assets/map-marker.png", "baloon_text": '<strong>PLANAR</strong> 1110 Bates Avenue Los Angeles, CA 90029, US'}
+				{"latitude": "-34.570067", "longitude":"-58.416178", "icon": "assets/map-marker.png", "baloon_text": '<strong>Palermo</strong>'},
+				{"latitude": "-34.515921", "longitude":"-58.469894", "icon": "assets/map-marker.png", "baloon_text": '<strong>Vicente Lopez</strong>'},
+				{"latitude": "-34.605976", "longitude":"-58.364450", "icon": "assets/map-marker.png", "baloon_text": '<strong>Puerto Madero</strong>'}
 			]
 		};
 
 		//set up map options
 		$("#map").mapmarker({
 			zoom	: 11,
-			center	: '1110 Bates Avenue Los Angeles, CA 90029, United States',
+			center	: 'Palermo, Buenos Aires, Argentina',
 			markers	: myMarkers
 		});
 
